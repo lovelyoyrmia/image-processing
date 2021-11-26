17da5c90e7344a1c4b245bc67c78b9a89628cd7d
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-11-26 19:05:35 +0700

    remove dotenv

--- detections.py
+++ detections.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
-body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
-
+try:
+   face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+   smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+except Exception as err:
+   print(err)
 
 def detect_faces(images):
    new_img = np.array(images.convert('RGB'))
@@ -12,7 +13,7 @@ def detect_faces(images):
    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
    # Detect Faces
-   faces = face_cascade.detectMultiScale(gray, 1.8, 2)
+   faces = face_cascade.detectMultiScale(gray, 1.8, 5)
    for (x, y, w, h) in faces:
       cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 4)
    return img, faces
@@ -27,7 +28,7 @@ def detect_smiles(images):
       cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
       the_face = img[y:y+h, x:x+w]
       face_gray_scale = cv2.cvtColor(the_face,cv2.COLOR_BGR2GRAY)
-      smile_detect = smile_cascade.detectMultiScale(face_gray_scale, 1.7,minNeighbors=2)
+      smile_detect = smile_cascade.detectMultiScale(face_gray_scale, 1.7,minNeighbors=25)
    
       if len(smile_detect) > 0:
          cv2.putText(img,'smiling',
