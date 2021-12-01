a8fc76b362113b453c9b22d7462e023a51551b2b
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-01 13:00:38 +0700

    features mask r-cnn

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import detections as dt
 import base64
 from bgRemoval import removeBG
 from PIL import Image, ImageEnhance
+from maskRcnn import maskImage
 
-hide_menu_style = '''
+hide_menu_style = """
    <style>
       #MainMenu {display: none; }
       footer {visibility: hidden;}
@@ -14,106 +15,134 @@ hide_menu_style = '''
       #stStatusWidget {display: none;}
       .css-r698ls {display: none;}
    </style>
-'''
+"""
 
-image_logo = Image.open('images.jpg')
-st.set_page_config(page_title='Image Procs', page_icon=image_logo, layout="wide")
+image_logo = Image.open("images.jpg")
+st.set_page_config(page_title="Image Procs", page_icon=image_logo, layout="wide")
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 @st.cache()
 def load_image(image_file):
-   img = Image.open(image_file)
-   return img
+    img = Image.open(image_file)
+    return img
+
 
 def main():
-   # ==== Image Processing ====
-   st.title('Image Processing App')
-   st.text('Build using streamlit and opencv')
+    # ==== Image Processing ====
+    st.title("Image Processing App")
+    st.text("Build using streamlit and opencv")
 
-   activities = ['Processing', 'About']
-   choice = st.sidebar.selectbox('Select Activity', activities)
+    activities = ["Processing", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activities)
 
-   if choice == 'Processing':
-      # st.subheader('Face Detection')
-      image_file = st.file_uploader('Upload Image', type=['png', 'jpg', 'jpeg'])
-      
+    if choice == "Processing":
+        # st.subheader('Face Detection')
+        image_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
 
-      if image_file:
-         
-         img = load_image(image_file)
-         
-         st.session_state.enchance_type = ['Original', 'Gray-Scale', 
-                                          'Contrast', 'Brightness', 'Blurring', 'Remove-Background']
-         st.session_state.enchance = st.sidebar.radio('Enchance Type', st.session_state.enchance_type)
+        if image_file:
 
-         if 'Original' in st.session_state.enchance:
-            st.subheader('Original')
-            st.image(img)
-            task = ['Original Image', 'Face Detection', 'Smile Detection']
-            option_task = st.sidebar.selectbox('Find Features', task)
-            if option_task == 'Original Image':
-               pass
+            img = load_image(image_file)
 
-            elif option_task == 'Face Detection':
-               if st.sidebar.button('Detect Faces'):
-                  result_img, faces = dt.detect_faces(img)
-                  st.subheader('Results')
-                  st.image(result_img)
-                  if len(faces) > 1:
-                     st.success(f'Found {len(faces)} Faces')
-                  else:
-                     st.success(f'Found {len(faces)} Face')
+            st.session_state.enchance_type = [
+                "Original",
+                "Gray-Scale",
+                "Contrast",
+                "Brightness",
+                "Blurring",
+                "Remove-Background",
+            ]
+            st.session_state.enchance = st.sidebar.radio(
+                "Enchance Type", st.session_state.enchance_type
+            )
 
-            elif option_task == 'Smile Detection':
-               if st.sidebar.button('Detect Smiles'):
-                  result_smile = dt.detect_smiles(img)
-                  st.subheader('Results')
-                  st.image(result_smile)
-                  
-         elif 'Gray-Scale' in st.session_state.enchance:
-            new_img = np.array(img.convert('RGB'))
-            img_cvt = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-            st.subheader('Gray-Scale')
-            st.image(img_cvt)
-            image_fromarray = Image.fromarray(img_cvt)
-            # image_decode = base64.b64encode(image_fromarray).decode('utf-8')
-            image_download = image_fromarray.tobytes()
-            st.download_button('Download Image', image_download)
-            
-            # print(image_download)
+            if "Original" in st.session_state.enchance:
+                st.subheader("Original")
+                st.image(img)
+                task = [
+                    "Original Image",
+                    "Face Detection",
+                    "Smile Detection",
+                    "Mask R-CNN Image",
+                ]
+                option_task = st.sidebar.selectbox(
+                    "Find Computer Vision Features", task
+                )
+                if option_task == "Original Image":
+                    pass
 
-         elif 'Contrast' in st.session_state.enchance:
-            c_rate = st.sidebar.slider('Contrast', 0.5, 3.5, step=.5, key='contrast')
-            enchancer = ImageEnhance.Contrast(img)
-            img_contrast = enchancer.enhance(c_rate)
-            st.subheader('Contrast')
-            st.image(img_contrast)
+                elif option_task == "Face Detection":
+                    if st.sidebar.button("Detect Faces"):
+                        result_img, faces = dt.detect_faces(img)
+                        st.subheader("Results")
+                        st.image(result_img)
+                        if len(faces) > 1:
+                            st.success(f"Found {len(faces)} Faces")
+                        else:
+                            st.success(f"Found {len(faces)} Face")
 
-         elif 'Brightness' in st.session_state.enchance:
-            c_rate = st.sidebar.slider('Brightness', 0.5, 3.5, step=.5, key='brightness')
-            enchancer = ImageEnhance.Brightness(img)
-            img_brightness = enchancer.enhance(c_rate)
-            st.subheader('Brightness')
-            st.image(img_brightness)
-         
-         elif 'Blurring' in st.session_state.enchance:
-            blur_rate = st.sidebar.slider('Blurring', 0.5, 3.5, step=.5, key='blurring')
-            new_img = np.array(img.convert('RGB'))
-            img_blur = cv2.GaussianBlur(new_img, (11, 11), blur_rate)
-            st.subheader('Blurring')
-            st.image(img_blur)
+                elif option_task == "Smile Detection":
+                    if st.sidebar.button("Detect Smiles"):
+                        result_smile = dt.detect_smiles(img)
+                        st.subheader("Results")
+                        st.image(result_smile)
+                else:
+                    if st.sidebar.button("Mask Image"):
+                        result_mask = maskImage(img)
+                        st.subheader("Results")
+                        st.image(result_mask)
 
-         else:
-            st.subheader('Original')
-            st.image(img)
-            new_img = np.array(img)
-            if st.sidebar.button('Remove Background'):
-               imgOut = removeBG(new_img)
-               st.subheader('Results')
-               st.image(imgOut)
+            elif "Gray-Scale" in st.session_state.enchance:
+                new_img = np.array(img.convert("RGB"))
+                img_cvt = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+                st.subheader("Gray-Scale")
+                st.image(img_cvt)
+                image_fromarray = Image.fromarray(img_cvt)
+                # image_decode = base64.b64encode(image_fromarray).decode('utf-8')
+                image_download = image_fromarray.tobytes()
+                st.download_button("Download Image", image_download)
 
-   else:
-      st.markdown('''
+                # print(image_download)
+
+            elif "Contrast" in st.session_state.enchance:
+                c_rate = st.sidebar.slider(
+                    "Contrast", 0.5, 3.5, step=0.5, key="contrast"
+                )
+                enchancer = ImageEnhance.Contrast(img)
+                img_contrast = enchancer.enhance(c_rate)
+                st.subheader("Contrast")
+                st.image(img_contrast)
+
+            elif "Brightness" in st.session_state.enchance:
+                c_rate = st.sidebar.slider(
+                    "Brightness", 0.5, 3.5, step=0.5, key="brightness"
+                )
+                enchancer = ImageEnhance.Brightness(img)
+                img_brightness = enchancer.enhance(c_rate)
+                st.subheader("Brightness")
+                st.image(img_brightness)
+
+            elif "Blurring" in st.session_state.enchance:
+                blur_rate = st.sidebar.slider(
+                    "Blurring", 0.5, 3.5, step=0.5, key="blurring"
+                )
+                new_img = np.array(img.convert("RGB"))
+                img_blur = cv2.GaussianBlur(new_img, (11, 11), blur_rate)
+                st.subheader("Blurring")
+                st.image(img_blur)
+
+            else:
+                st.subheader("Original")
+                st.image(img)
+                new_img = np.array(img)
+                if st.sidebar.button("Remove Background"):
+                    imgOut = removeBG(new_img)
+                    st.subheader("Results")
+                    st.image(imgOut)
+
+    else:
+        st.markdown(
+            """
          ### Follow me on \n
         
          <div class='icon-container'>
@@ -143,7 +172,10 @@ def main():
                margin-top: 2rem;
             }
          </style>
-      ''', unsafe_allow_html=True)
+      """,
+            unsafe_allow_html=True,
+        )
 
-if __name__ == '__main__':
-   main()
+
+if __name__ == "__main__":
+    main()
