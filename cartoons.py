28f921a7cfb36ebcad8c20bd5eb78da41a544804
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-03 17:58:21 +0700

    add function cartoonize

--- cartoons.py
+++ cartoons.py
@@ -1,25 +1,27 @@
 import cv2
 import numpy as np
+import streamlit as st
 from emaskRcnn import loadImage
 
 
 def colorQuantization(image, k):
-    # new_img = loadImage(image)
-    data = np.float32(image).reshape((-1, 3))
+    new_img = loadImage(image)
+    # img_cvt = cv2.cvtColor(new_img, 1)
+    data = np.float32(new_img).reshape((-1, 3))
     creteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
     ret, label, center = cv2.kmeans(
         data, k, None, creteria, 10, cv2.KMEANS_RANDOM_CENTERS
     )
     center = np.uint8(center)
     result = center[label.flatten()]
-    result = result.reshape(image.shape)
+    result = result.reshape(new_img.shape)
 
     return result
 
 
 def edgeMask(image, line_size, blur_value):
-    # new_img = loadImage(image)
-    img_cvt = cv2.cvtColor(image, 1)
+    new_img = loadImage(image)
+    img_cvt = cv2.cvtColor(new_img, 1)
     gray = cv2.cvtColor(img_cvt, cv2.COLOR_BGR2GRAY)
     gray_blur = cv2.medianBlur(gray, blur_value)
     edges = cv2.adaptiveThreshold(
@@ -33,18 +35,13 @@ def edgeMask(image, line_size, blur_value):
     return edges
 
 
-img = cv2.imread("vio.jpg")
+def cartoonize(img):
+    blur_value = st.sidebar.slider("Blur Value", 7, 10, key="blur_value")
+    line_size = 7
+    total_color = st.sidebar.slider("Total Color", 7, 15, key="line_size")
 
-line_size = 7
-blur_value = 7
-
-edges = edgeMask(img, line_size, blur_value)
-
-total_color = 11
-img = colorQuantization(img, total_color)
-cv2.imshow("", img)
-cv2.waitKey(0)
-
-blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
-
-cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
+    edges = edgeMask(img, line_size, blur_value)
+    img_quantization = colorQuantization(img, total_color)
+    blurred = cv2.bilateralFilter(img_quantization, d=7, sigmaColor=200, sigmaSpace=200)
+    cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
+    return edges, img_quantization, blurred, cartoon
