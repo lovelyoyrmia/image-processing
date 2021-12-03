ed4a69e50d37306922ce5c36f6a7b12ddeb68b75
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-03 11:11:48 +0700

    testing function cartoonize

--- cartoons.py
+++ cartoons.py
@@ -3,7 +3,8 @@ import numpy as np
 from emaskRcnn import loadImage
 
 
-def color_quantization(image, k):
+def colorQuantization(image, k):
+    # new_img = loadImage(image)
     data = np.float32(image).reshape((-1, 3))
     creteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
     ret, label, center = cv2.kmeans(
@@ -16,9 +17,10 @@ def color_quantization(image, k):
     return result
 
 
-def edge_mask(image, line_size, blur_value):
+def edgeMask(image, line_size, blur_value):
     # new_img = loadImage(image)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img_cvt = cv2.cvtColor(image, 1)
+    gray = cv2.cvtColor(img_cvt, cv2.COLOR_BGR2GRAY)
     gray_blur = cv2.medianBlur(gray, blur_value)
     edges = cv2.adaptiveThreshold(
         gray_blur,
@@ -36,19 +38,13 @@ img = cv2.imread("vio.jpg")
 line_size = 7
 blur_value = 7
 
-edges = edge_mask(img, line_size, blur_value)
-# cv2.imshow("", edges)
-# cv2.waitKey(0)
+edges = edgeMask(img, line_size, blur_value)
 
 total_color = 11
-img = color_quantization(img, total_color)
-# cv2.imshow("", img)
-# cv2.waitKey(0)
-
-blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
-cv2.imshow("", blurred)
+img = colorQuantization(img, total_color)
+cv2.imshow("", img)
 cv2.waitKey(0)
 
+blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
+
 cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
-# cv2.imshow("", cartoon)
-# cv2.waitKey(0)
