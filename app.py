bbb142aee6acc173f2b93e32db30bc6bd981108e
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-01-22 11:43:38 +0700

    cleaning code

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import numpy as np
-from about import aboutPage
-import detections as dt
 import time
-from modules import Features, Download
+from modules import *
 from PIL import Image
-from bgRemoval import removeBG
-from emaskRcnn import maskImage
-from sendEmail import sendEmail
-from components import imageSt, imageSidebar
 
 
 @st.cache()
@@ -17,10 +11,140 @@ def loadImagePIL(image_file):
     return img
 
 
+def processing():
+    image_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
+
+    if image_file is not None:
+
+        component = Components(st)
+        download = Download(st)
+
+        img = loadImagePIL(image_file)
+
+        features = Features(st, img)
+
+        st.session_state.enchance_type = [
+            "Original",
+            "Gray-Scale",
+            "Contrast",
+            "Brightness",
+            "Blurring",
+            "Cartoonize",
+            "Remove-Background",
+        ]
+
+        st.session_state.enchance = st.sidebar.radio(
+            "Enchance Type", st.session_state.enchance_type
+        )
+
+        if "Original" in st.session_state.enchance:
+            detect = Detection(st)
+            mask = MaskRcnn(st)
+
+            task = [
+                "Original Image",
+                "Face Detection",
+                "Smile Detection",
+                "Body and Object Detection",
+                "Mask R-CNN Image",
+            ]
+
+            option_task = st.sidebar.selectbox("Find Computer Vision Features", task)
+
+            if option_task == "Original Image":
+                component.imageSt(img, "Original Image")
+
+            elif option_task == "Face Detection":
+                if st.sidebar.button("Detect Faces"):
+                    with st.spinner("Loading..."):
+                        time.sleep(2)
+                        result_img, faces = detect.detectFaces(img)
+                        component.imageSt(result_img, "Result")
+                        component.imageSidebar(img)
+                        if len(faces) > 1:
+                            st.success(f"Found {len(faces)} Faces")
+                        else:
+                            st.success(f"Found {len(faces)} Face")
+                else:
+                    component.imageSt(img, "Original Image")
+
+            elif option_task == "Smile Detection":
+                if st.sidebar.button("Detect Smiles"):
+                    with st.spinner("Loading..."):
+                        time.sleep(2)
+                        result_smile = detect.detectSmiles(img)
+                        component.imageSt(result_smile, "Result")
+                        component.imageSidebar(img)
+                else:
+                    component.imageSt(img, "Original Image")
+
+            elif option_task == "Body and Object Detection":
+                if st.sidebar.button("Detect Bodies & Objects"):
+                    with st.spinner("Loading..."):
+                        time.sleep(2)
+                        result_obj, _ = mask.maskImage(img)
+                        component.imageSt(result_obj, "Result")
+                        component.imageSidebar(img)
+                else:
+                    component.imageSt(img, "Original Image")
+
+            else:
+                if st.sidebar.button("Mask Image"):
+                    with st.spinner("Loading..."):
+                        time.sleep(2)
+                        _, result_mask = mask.maskImage(img)
+                        component.imageSt(result_mask, "Mask Image")
+                        img_bytes = download.downloader(result_mask)
+                        download.imageDownloader(img_bytes)
+                        component.imageSidebar(img)
+                else:
+                    component.imageSt(img, "Original Image")
+
+        elif "Gray-Scale" in st.session_state.enchance:
+            img_cvt_grey = features.greyscaleFeatures()
+            component.imageSt(img_cvt_grey, "Gray Scale")
+            img_bytes = download.downloader(img_cvt_grey)
+            download.imageDownloader(img_bytes)
+
+        elif "Contrast" in st.session_state.enchance:
+            image_contrast = features.contrastFeatures()
+            image_convert_arr = download.imageConvertArray(image_contrast)
+            component.imageSt(image_convert_arr, "Contrast")
+            img_bytes = download.downloader(image_convert_arr)
+            download.imageDownloader(img_bytes)
+
+        elif "Brightness" in st.session_state.enchance:
+            image_brightness = features.brightnessFeatures()
+            image_convert_arr = download.imageConvertArray(image_brightness)
+            component.imageSt(image_convert_arr, "Brightness")
+            img_bytes = download.downloader(image_convert_arr)
+            download.imageDownloader(img_bytes)
+
+        elif "Blurring" in st.session_state.enchance:
+            img_blurring = features.blurrFeatures()
+            component.imageSt(img_blurring, "Blurring")
+            img_bytes = download.downloader(img_blurring)
+            download.imageDownloader(img_bytes)
+
+        elif "Cartoonize" in st.session_state.enchance:
+            features.cartoonFeatures()
+
+        else:
+            remove = RemoveBg()
+            new_img = np.array(img)
+            if st.sidebar.button("Remove Background"):
+                img_rmbg = remove.removeBG(new_img)
+                component.imageSt(img_rmbg, "Result")
+                component.imageSidebar(img)
+                img_bytes = download.downloader(img_rmbg)
+                download.imageDownloader(img_bytes)
+            else:
+                component.imageSt(img, "Original Image")
+
+
 def main():
     image_logo = Image.open("assets/images.jpg")
-    st.set_page_config(page_title="Image Procs",
-                       page_icon=image_logo, layout="wide")
+    st.set_page_config(page_title="Image Procs", page_icon=image_logo, layout="wide")
     hide_menu_style = """
     <style>
         #MainMenu {display: none; }
@@ -36,168 +160,25 @@ def main():
     st.title("Image Processing App")
     st.text("Build using Computer Vision and Artificial Intelligence")
 
-    activities = ["Processing", "Subscribe", "About"]
-    choice = st.sidebar.selectbox("Select Activity", activities)
+    activities = ["Welcome", "Processing", "Get In Touch", "About"]
+    navigation = st.sidebar.selectbox("Select Activity", activities)
+    nav = Navigation(st)
 
-    # === Subscribe Page ===
-    if choice == "Subscribe":
-        st.subheader("Let's get in touch")
-        st.image("assets/images.jpg", width=400)
-        email = st.text_input("Enter your email")
-        if st.button("Subscribe"):
-            with st.spinner("Sending email..."):
-                sendEmail(email)
+    # === Welcome Page ===
+    if navigation == "Welcome":
+        nav.welcome()
 
     # === Processing Page ===
-    elif choice == "Processing":
+    elif navigation == "Processing":
+        processing()
 
-        # st.subheader('Face Detection')
-        image_file = st.file_uploader(
-            "Upload Image", type=["png", "jpg", "jpeg"])
-
-        if image_file is not None:
-
-            img = loadImagePIL(image_file)
-
-            features = Features(st, img)
-
-            download = Download(st)
-
-            img_grey = None
-            img_contrast = None
-            img_blur = None
-            img_bright = None
-
-            st.session_state.enchance_type = [
-                "Original",
-                "Gray-Scale",
-                "Contrast",
-                "Brightness",
-                "Blurring",
-                "Cartoonize",
-                "Remove-Background",
-            ]
-            st.session_state.enchance = st.sidebar.radio(
-                "Enchance Type", st.session_state.enchance_type
-            )
-
-            if "Original" in st.session_state.enchance:
-
-                task = [
-                    "Original Image",
-                    "Face Detection",
-                    "Smile Detection",
-                    "Body and Object Detection",
-                    "Mask R-CNN Image",
-                ]
-                option_task = st.sidebar.selectbox(
-                    "Find Computer Vision Features", task
-                )
-                if option_task == "Original Image":
-                    st.subheader("Original Image")
-                    imageSt(img)
-
-                elif option_task == "Face Detection":
-                    if st.sidebar.button("Detect Faces"):
-                        with st.spinner("Loading..."):
-                            time.sleep(2)
-                            result_img, faces = dt.detect_faces(img)
-                            st.subheader("Results")
-                            imageSt(result_img)
-                            imageSidebar(img)
-                            if len(faces) > 1:
-                                st.success(f"Found {len(faces)} Faces")
-                            else:
-                                st.success(f"Found {len(faces)} Face")
-                    else:
-                        st.subheader("Original Image")
-                        imageSt(img)
-
-                elif option_task == "Smile Detection":
-                    if st.sidebar.button("Detect Smiles"):
-                        with st.spinner("Loading..."):
-                            time.sleep(2)
-                            result_smile = dt.detect_smiles(img)
-                            st.subheader("Results")
-                            imageSt(result_smile)
-                            imageSidebar(img)
-                    else:
-                        st.subheader("Original Image")
-                        imageSt(img)
-
-                elif option_task == "Body and Object Detection":
-                    if st.sidebar.button("Detect Bodies & Objects"):
-                        with st.spinner("Loading..."):
-                            time.sleep(2)
-                            result_obj, _ = maskImage(img)
-                            st.subheader("Results")
-                            imageSt(result_obj)
-                            imageSidebar(img)
-                    else:
-                        st.subheader("Original Image")
-                        imageSt(img)
-
-                else:
-                    if st.sidebar.button("Mask Image"):
-                        with st.spinner("Loading..."):
-                            time.sleep(2)
-                            _, result_mask = maskImage(img)
-                            st.subheader("Masks")
-                            imageSt(result_mask)
-                            imageSidebar(img)
-                    else:
-                        st.subheader("Original Image")
-                        imageSt(img)
-
-            elif "Gray-Scale" in st.session_state.enchance:
-                img_cvt_grey = features.greyscaleFeatures()
-                st.subheader("Gray-Scale")
-                imageSt(img_cvt_grey)
-                img_bytes = download.downloader(img_cvt_grey)
-                download.imageDownloader(img_bytes)
-
-            elif "Contrast" in st.session_state.enchance:
-                image_contrast = features.contrastFeatures()
-                image_convert_arr = download.imageConvertArray(image_contrast)
-                st.subheader("Contrast")
-                imageSt(image_convert_arr)
-                img_bytes = download.downloader(image_convert_arr)
-                download.imageDownloader(img_bytes)
-
-            elif "Brightness" in st.session_state.enchance:
-                image_brightness = features.brightnessFeatures()
-                image_convert_arr = download.imageConvertArray(image_brightness)
-                st.subheader("Brightness")
-                imageSt(image_convert_arr)
-                img_bytes = download.downloader(image_convert_arr)
-                download.imageDownloader(img_bytes)
-
-            elif "Blurring" in st.session_state.enchance:
-                img_blurring = features.blurrFeatures()
-                st.subheader("Blurring")
-                imageSt(img_blurring)
-                img_bytes = download.downloader(img_blurring)
-                download.imageDownloader(img_bytes)
-
-            elif "Cartoonize" in st.session_state.enchance:
-                features.cartoonFeatures()
-
-            else:
-                new_img = np.array(img)
-                if st.sidebar.button("Remove Background"):
-                    img_rmbg = removeBG(new_img)
-                    st.subheader("Results")
-                    imageSt(img_rmbg)
-                    imageSidebar(img)
-                    img_bytes = download.downloader(img_rmbg)
-                    download.imageDownloader(img_bytes)
-                else:
-                    st.subheader("Original")
-                    imageSt(img)
+    # === Get In Touch Page ===
+    elif navigation == "Get In Touch":
+        nav.contact()
 
     # === About Page ===
     else:
-        aboutPage(st)
+        nav.aboutPage()
 
 
 if __name__ == "__main__":
--- components.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def imageSt(image):
-    st.image(image, use_column_width=True)
-
-
-def imageSidebar(image):
-    st.sidebar.subheader('Original')
-    st.sidebar.image(image, use_column_width=True)
