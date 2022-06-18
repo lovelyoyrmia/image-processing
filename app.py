4b507ecf5b4f1a05d02286942d93fdd9d549d48b
Lovelyo Yeremia Mokalu <91578003+lovelyoyrmia@users.noreply.github.com>
2022-06-18 09:35:45 +0700

    update logo app

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ def computerVisionFeatures():
 
 
 def main():
-    image_logo = Image.open("assets/logo.png")
+    image_logo = Image.open("assets/logo_app.png")
     st.set_page_config(page_title="Image Procs", page_icon=image_logo, layout="wide")
     hide_menu_style = """
     <style>
